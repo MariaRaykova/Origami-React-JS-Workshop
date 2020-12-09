ab77b65caad46a5070d94c6e35c1e969e534bf31
Maria Raykova <66566569+MariaRaykova@users.noreply.github.com>
2020-12-09 17:43:00 +0200

    feat: origamis container

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import styles from './app.module.css';
+import styles from './app.module.css'
 import Header from './components/header'
+import Aside from './components/aside'
+import Origamis from './components/origamis'
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <div className={styles.container}> 
+        <Aside/>
+        <Origamis />
+      </div>
     </div>
   );
 }
---
+++ src/components/origami/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styles from './index.module.css';
+
+
+const Origami = ({ description }) => {
+
+
+    return (
+        <div> 
+           {description} 
+        </div>
+    )
+}
+export default Origami
