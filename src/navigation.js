73940b0e4c531787278cda4ae0bdc3e500ef93b5
Maria Raykova <66566569+MariaRaykova@users.noreply.github.com>
2020-12-13 21:52:21 +0200

    feat: register and login page

--- src/navigation.js
+++ src/navigation.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Publications from './pages/publications'
 import ShareThoughtsPage from './pages/share-thoughts-page'
 //import ProfilePage from './pages/profile-pagee'
-//import RegisterPage from './pages/register-page'
-//import LoginPage from './pages/login-page'
+import RegisterPage from './pages/register-page'
+import LoginPage from './pages/login-page'
 
 const Navigation = () => {
     return (
@@ -12,9 +12,9 @@ const Navigation = () => {
             <Switch>
                 <Route path="/" exact component={Publications} />
                 <Route path="/share" component={ShareThoughtsPage} />
-                {/* <Route path="/profile" component={ProfilePage} />
+                 {/* <Route path="/profile" component={ProfilePage} /> */}
                 <Route path="/register" component={RegisterPage} />
-                <Route path="/login" component={LoginPage} /> */}
+                <Route path="/login" component={LoginPage} /> 
             </Switch>
         </BrowserRouter>
     )
